f6e7c128e27e63e500af9250d3a1d04c300661bb
Ad Taylor <ad@adtaylor.co.uk>
2014-09-05 12:39:59 +0100

    Adds server init code

--- app.js
+++ app.js
@@ -59,5 +59,8 @@ server.init();
 //     });
 // });
 
-
-// module.exports = app;
+// app.set('port', process.env.PORT || 3000);
+//
+// var server = app.listen(app.get('port'), function() {
+//   debug('Express server listening on port ' + server.address().port);
+// });
